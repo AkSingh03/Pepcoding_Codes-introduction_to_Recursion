fb1fd0e05f6e6ccbbcb362520cb4140d20716ad5
Akjot Singh <51170427+AkSingh03@users.noreply.github.com>
2021-10-22 02:07:07 +0530

    Create Power-linear.java

---
+++ Power-linear.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    // write your code here
+
+    Scanner scn = new Scanner(System.in);
+
+    int x = scn.nextInt();
+
+    int n = scn.nextInt();
+
+    int xpn = power(x, n);
+
+    System.out.println(xpn);
+
+
+  }
+
+  public static int power(int x, int n) {
+
+    if (n == 0) {
+
+      return 1 ;
+
+    }
+
+    int xpnm1 = power( x , n - 1);
+
+    int xpn =  xpnm1 * x ;
+
+    return xpn ;
+
+  }
+
+}

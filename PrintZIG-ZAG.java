47ba61b05e1228c75c779fe9ed3adbc6413d5d36
Akjot Singh <51170427+AkSingh03@users.noreply.github.com>
2021-10-22 01:32:40 +0530

    Create PrintZIG-ZAG.java

---
+++ PrintZIG-ZAG.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner Scn = new Scanner(System.in);
+        
+        int n = Scn.nextInt();
+        
+        pzz(n);
+        
+    }
+
+    public static void pzz(int n){
+        
+        if (n == 0){
+            
+            return ;
+            
+        }
+        
+        System.out.print( n + " " );
+        
+        pzz(n-1);
+        
+        System.out.print( n + " ");
+        
+        pzz(n-1);
+        
+        System.out.print( n  + " ");
+        
+    }
+
+}
